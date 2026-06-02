7303175e20fa333b42e666105b192e1d5c4282c0
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-06-02 13:11:13 +0530

    Add Variables class with main method and variable examples

---
+++ chapter2/Variables.java
@@ -0,0 +1,15 @@
+
+
+public class Variables {
+    public static void main(String[] args){
+        int age = 21;
+        double marks = 99.9;
+        char garde = 'A';
+        boolean isPlaced = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(garde);
+        System.out.println(isPlaced);
+    }
+}
