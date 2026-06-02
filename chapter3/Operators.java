3d0b534cc5826be66fb81568f2bd4a9b0e7368ce
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-06-03 01:42:59 +0530

    Add Operators class and chapter notes for Operators in Java

---
+++ chapter3/Operators.java
@@ -0,0 +1,42 @@
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+        System.out.println(a);
+        int b = 10;
+
+        System.out.print(b);
+        System.out.print(++b);
+        System.out.print(b++);
+        System.out.print(++b);
+        System.out.print(b++);
+        System.out.print(b);
+
+        // System.out.println(true && true);
+        // System.out.println(true && false);
+        // System.out.println(false && true);
+        // System.out.println(false && false);
+
+        // System.out.println(true || true);
+        // System.out.println(true || false);
+        // System.out.println(false || true);
+        // System.out.println(false || false);
+
+        // System.out.println(!true);
+        // System.out.println(!false);
+
+        // System.out.println(a == b);
+        // System.out.println(a != b);
+        // System.out.println(a > b);
+        // System.out.println(a < b);
+        // System.out.println(a <= b);
+        // System.out.println(a >= b);
+
+        // System.out.println(a + b);
+        // System.out.println(a - b);
+        // System.out.println(a * b);
+        // System.out.println(a / b);
+        // System.out.println(a % b);
+    }
+}
